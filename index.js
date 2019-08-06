f8731cfadeea4f3f3195f7b83af59faf724ae3fc
Donald Lobree <edl1155@gmail.com>
2019-08-06 03:34:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(arrayMus, arrayInst) {
+  var newArr = [];
+  for (let i = 0; i < arrayMus.length; i++) {
+    newArr.push(`${arrayMus[i]} plays ${arrayInst[i]}`);
+  }
+  return newArr;
+}
